d565d57f0e1f1d121ffcabe82cabbf13de2bc6d0
Elisa Young <esyoung@mit.edu>
2018-03-04 22:40:41 -0800

    New site layout and design

--- js/script-single.js
+++
@@ -1,9 +0,0 @@
-// hardcoded to test
-
-for(i = 0; i < 9; i++) {
-    var photopath = "photos/temp-";
-    var extension = ".jpg";
-    var partA = "<img class='photo' src=";
-    var partB = "/>";
-    $(".test-single").append(partA+photopath+i+extension+partB);
-}
